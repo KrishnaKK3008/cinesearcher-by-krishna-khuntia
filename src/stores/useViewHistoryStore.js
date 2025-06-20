b65b4a636b62522e6f90e476383cab234e1161f5
KrishnaKK3008 <krishnakumarkhuntia3008@gmail.com>
2025-06-20 12:24:32 +0530

    Changed Fallback Image

--- src/stores/useViewHistoryStore.js
+++ src/stores/useViewHistoryStore.js
@@ -1,25 +1,17 @@
 import { uniqBy, pipe, prepend } from "ramda";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
-const useViewHistoryStore = create(
-  persist(
-    set => ({
-      movies: [],
-      selectedMovie: null,
-      setMovies: movie =>
-        set(state => ({
-          movies: pipe(
-            prepend(movie),
-            uniqBy(m => m.imdbID)
-          )(state.movies),
-        })),
-      setSelectedMovie: movie => set({ selectedMovie: movie }),
-    }),
-    {
-      name: "movie-history-storage",
-    }
-  )
-);
+const useViewHistoryStore = create(set => ({
+  movies: [],
+  selectedMovie: null,
+  setMovies: movie =>
+    set(state => ({
+      movies: pipe(
+        prepend(movie),
+        uniqBy(m => m.imdbID)
+      )(state.movies),
+    })),
+  setSelectedMovie: movie => set({ selectedMovie: movie }),
+}));
 
 export default useViewHistoryStore;
